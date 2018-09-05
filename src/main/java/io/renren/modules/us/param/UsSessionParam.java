7dc0dbcefb9d4f7d29edb270c52665f5e7f87207
lichao@gophagroup.com <lichao@gophagroup.com>
2018-09-05 16:27:58 +0800

    修改过期API.

--- src/main/java/io/renren/modules/us/param/UsSessionParam.java
+++ src/main/java/io/renren/modules/us/param/UsSessionParam.java
@@ -1,7 +1,6 @@
 package io.renren.modules.us.param;
 
-import io.swagger.annotations.ApiModelProperty;
-import org.hibernate.validator.constraints.NotBlank;
+import javax.validation.constraints.NotBlank;
 
 public class UsSessionParam extends UsBaseParam {
 
